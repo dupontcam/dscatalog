95692a8577074990935b4c20741c0a852b0183cd
dupontcam <125046483+dupontcam@users.noreply.github.com>
2024-02-15 08:40:21 -0300

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dscatalog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests(req -> {
+            req.requestMatchers("/h2-console").permitAll();
+            req.requestMatchers("/h2-console/**").permitAll();
+        });
+        http.authorizeHttpRequests(req -> req.requestMatchers("/h2-console/").permitAll());
+        http.authorizeHttpRequests(req -> req.anyRequest().permitAll());
+        http.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()));
+        return http.build();
+    }
+}
